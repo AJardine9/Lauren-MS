d3cff6525f17fe144cef59903f3e8d251be3d181
Lauren <laurennbenson@hotmail.com>
2019-01-16 16:49:54 -0500

    Setup Some Classes for MVC

---
+++ PizzaStore/PizzaStore.Client/Controllers/LocationsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaStore.Client.Controllers
+{
+    [Route("[controller]")]
+    public class LocationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return;
+        }
+    }
+}
